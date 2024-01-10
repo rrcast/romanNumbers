92d4bfd4ff067334918b3b1b8cbc3e3762e7f639
Ricardo castillo <ricardocastillo@Ricardos-Mac-mini.local>
2024-01-09 21:55:44 -0600

    first commit

---
+++ src/main/java/org/example/RomanNumbers.java
@@ -0,0 +1,27 @@
+package org.example;
+
+
+/**
+ So, create a project on a publicly hosted git repository (e.g. github) and implement:
+
+
+
+ With any integer, show the roman numeral;
+
+ With any roman numeral, show the integer.
+
+
+
+ Please at least do part 1 (at most up to 3000) and if it's still fun and you've got time, feel free to go on to part 2.
+
+
+
+ Don't worry about how far you get... we're more interested in you reaching the point of a well factored Part 1 that goes to 100 than a hacked together Part 1 & 2 that goes to 3000.”
+ */
+public class RomanNumbers {
+    public static void main(String[] args) {
+
+       System.out.println("Hi");
+
+    }
+}
---
+++ src/test/java/RomanNumbersTest.java
@@ -0,0 +1,36 @@
+import org.junit.Test;
+
+import static org.example.RomanNumbers.*;
+import static org.junit.Assert.assertEquals;
+/*
+
+ So, create a project on a publicly hosted git repository (e.g. github) and implement:
+
+
+
+ With any integer, show the roman numeral;
+
+ With any roman numeral, show the integer.
+
+
+
+ Please at least do part 1 (at most up to 3000) and if it's still fun and you've got time, feel free to go on to part 2.
+
+
+
+ Don't worry about how far you get... we're more interested in you reaching the point of a well factored Part 1 that goes to 100 than a hacked together Part 1 & 2 that goes to 3000.”
+ */
+
+
+public class RomanNumbersTest {
+    @Test
+    public void testIntToRoman(){
+
+
+    }
+
+
+
+
+
+}
